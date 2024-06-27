13c90d13e029a921ebb75e3adc24590465e07dd9
jambo <mijac0624@qq.com>
2024-06-27 11:33:12 +0100

    feat: add small model to make the feat easier

--- dev-server/index.js
+++ dev-server/index.js
@@ -1,5 +1,9 @@
-import map from '../docs/_static/example_data/S5_iJO1366.Glycolysis_PPP_AA_Nucleotides.json'
-import model from '../docs/_static/example_data/iJO1366.json'
+// import map from '../docs/_static/example_data/S5_iJO1366.Glycolysis_PPP_AA_Nucleotides.json'
+import map from '../docs/_static/example_data/RECON1_FIT_data.json'
+// import model from '../docs/_static/example_data/iJO1366.json'
+import model from '../docs/_static/example_data/PPPmodel.json'
+// import reaction_data from '../docs/_static/example_data/all_csv_data.json'
+import reaction_data from '../docs/_static/example_data/related_data.json'
 import { Builder, libs } from '../src/main'
 
 window.builder = new Builder( // eslint-disable-line no-new
@@ -9,6 +13,8 @@ window.builder = new Builder( // eslint-disable-line no-new
   libs.d3_select('#root'),
   {
     fill_screen: true,
-    never_ask_before_quit: true
+    never_ask_before_quit: true,
+    scroll_behavior: 'zoom',
+    reaction_data
   }
 )
