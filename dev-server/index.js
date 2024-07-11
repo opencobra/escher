2c916fdf6967e8ccf1446b2c7a3041ad353dc056
jambo <mijac0624@qq.com>
2024-07-11 16:21:38 +0100

    revert: index.js

--- dev-server/index.js
+++ dev-server/index.js
@@ -1,10 +1,5 @@
-// import map from '../docs/_static/example_data/S5_iJO1366.Glycolysis_PPP_AA_Nucleotides.json'
-import map from '../docs/_static/example_data/RECON1_FIT_data.json'
-// import map from '../docs/_static/example_data/escher-v4.json'
-// import model from '../docs/_static/example_data/iJO1366.json'
-import model from '../docs/_static/example_data/PPPmodel.json'
-import reaction_data from '../docs/_static/example_data/all_csv_data.json'
-// import reaction_data from '../docs/_static/example_data/related_data.json'
+import map from '../docs/_static/example_data/S5_iJO1366.Glycolysis_PPP_AA_Nucleotides.json'
+import model from '../docs/_static/example_data/iJO1366.json'
 import { Builder, libs } from '../src/main'
 
 window.builder = new Builder( // eslint-disable-line no-new
@@ -14,9 +9,6 @@ window.builder = new Builder( // eslint-disable-line no-new
   libs.d3_select('#root'),
   {
     fill_screen: true,
-    never_ask_before_quit: true,
-    scroll_behavior: 'zoom',
-    reaction_data,
-    show_reaction_data_animation: true
+    never_ask_before_quit: true
   }
 )
