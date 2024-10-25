e4ba347d6dc0d1a485db3e6565875d1080033cdf
jambo <mijac0624@qq.com>
2024-10-25 09:59:50 +0100

    fix: read the docs configuration

--- py/escher/version.py
+++ py/escher/version.py
@@ -28,6 +28,6 @@ __schema_version__ = package['schema_version']
 __map_model_version__ = package['map_model_version']
 
 # post-release version (required by PyPI & PEP440)
-__post_version__ = package['post_version']
+__post_version__ = package.get('post_version', None)
 # full version with post-release
 __full_version__ = get_full_version(__version__, __post_version__)
